d04fcb5e54f56c091f8d73d5eacead6316476406
Hello-GBY <nameGBY@163.com>
2023-02-01 22:15:28 +0800

    init

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,42 @@
+const nav = [
+    // 1.0 仅显示标题到导航栏
+    {
+        text: '首页',
+        link: '/'
+    },
+    // 2.0 显示标题到导航栏，且显示下拉标题
+    {
+        text: '关于作者',
+        items: [{
+                text: 'GitHub',
+                link: '',
+            },
+            {
+                text: '掘金',
+                link: '',
+            }
+        ]
+    },
+    // 3.0 显示标题到导航栏，且分类别显示下拉标题
+    {
+        text: '面试笔记',
+        items: [{
+                text: '基础',
+                //此处/以docs为根目录
+                items: [{
+                        text: 'Javascript',
+                        link: '/前端/01.Javascript/01.正则表达式',
+                        // sidebarDepth: 0,
+                    },
+                    {
+                        text: 'ES6',
+                        link: '/PAGE/2019-10-14-js-reg/',
+                        // link: '/前端/02.ES6/',
+                    },
+                ]
+            },
+        ]
+    }
+]
+
+module.exports = nav;
---
+++ docs/README.md
@@ -0,0 +1,30 @@
+---
+home: true
+heroImage: /img/favorite.png
+heroText: 郭兵宇的博客
+tagline: 好记性不如烂笔头
+actionText: 点击进入 →
+actionLink: /前端/01.Javascript/01.正则表达式
+features:
+- title: 记录
+  details: 记录过程。
+- title: 查阅
+  details: 便于查阅。
+- title: 分享
+  details: 分享知识。
+footer: MIT Licensed | Copyright © 2023-present guobingyu-blog
+---
+
+## 🌐 最近更新
+
+- **大前端**
+  - [《01.正则表达式》](http://www.almx.top/blog/PAGE/2019-10-14-js-reg)
+
+- **练习**
+  - [《01.扫雷》](http://www.almx.top/blog/PAGE/2019-10-23-sweep)
+
+- **工具**
+  - [《01.GitHub贡献不增加》](http://www.almx.top/blog/PAGE/2019-10-23-git-contribution)
+
+- **冲浪**
+  - [《01.GitHub贡献不增加》](http://www.almx.top/blog/PAGE/2019-10-23-git-contribution)
