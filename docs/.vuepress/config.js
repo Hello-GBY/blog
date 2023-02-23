7cdf9ec85649b8f252270a74246ffee32ff8b63d
GuoBingYu <nameGBY@163.com>
2023-02-23 22:09:22 +0800

    feat(config): 整加最后更新日期

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -40,7 +40,20 @@ export default defineUserConfig({
     sidebar, //侧边栏
     repo: 'https://github.com/Hello-GBY',
     repoLabel: 'GitHub',
+    displayAllHeaders: false, //展开侧边栏所有的小标题
+    sidebarDepth: 1, //提取markdown中h2标题，作为小标题显示在侧边栏上。
+    lastUpdated: '最后更新', // 文档更新时间：每个文件git最后提交的时间,
   }),
+  head: [
+    //         // 这里的 '/' 指向 docs/.vuepress/public 文件目录 
+      ['link', {
+          rel: 'icon',
+          href: '/img/witch.ico' //配置网页的小图标，如下图
+      }]
+  ],
+    markdown: {
+        lineNumbers: true // 代码块显示行号
+    },
   description: '这是我的第一个 VuePress 站点',
 })
 
