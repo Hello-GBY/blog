7b0fa8c2528217cb5d620a294e89b6fb8bd8ba60
GuoBingYu <nameGBY@163.com>
2023-02-24 22:41:16 +0800

    feat(sidebar): 侧边栏配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,66 +1,61 @@
-const navbar = require('./config/nav'); //引入导航栏配置
-const sidebar = require('./config/sidebar'); //引入侧边栏配置
+const navbar = require("./config/nav"); //引入导航栏配置
+const sidebar = require("./config/sidebar"); //引入侧边栏配置
 // const routeConfig = require('./plugins/routeConfig'); //引入路由配置
-import { defineUserConfig } from 'vuepress'
-import { defaultTheme } from 'vuepress'
-
-
+import { defineUserConfig } from "vuepress";
+import { defaultTheme } from "vuepress";
 
 export default defineUserConfig({
-  lang: 'zh-CN',
-  base: '/blog/', // 部署到github会用到的配置
+  lang: "zh-CN",
+  base: "/blog/", // 部署到github会用到的配置
   title: "兵宇的博客",
   theme: defaultTheme({
     // 默认主题配置
     // navbar, //导航
-    navbar: [
-        {
-            text: '一起进步',
-            link: '/一起进步/一起进步.md'
-        },
-        {
-            text: '企业级实战经验',
-            link: '/企业级实战经验/企业级实战经验.md'
-        },
-        {
-            text: '源码原理',
-            link: '/源码原理/源码原理.md'
-        },
-        // NavbarGroup
-        {
-            text: '个人知识库',
-            link: '/个人知识库/个人知识库.md'
-        },
-        // NavbarGroup
-        {
-            text: '读书笔记',
-            link: '/读书笔记/读书笔记.md'
-        }
-    ],
-    sidebar, //侧边栏
-    repo: 'https://github.com/Hello-GBY',
-    repoLabel: 'GitHub',
+    navbar,
+    sidebar: {
+        '/dev-experience/': [
+            {
+              text: 'CSS解决方案',
+              link: './css解决方案/css解决方案.md',
+              children: [ {
+                text: 'CSS规范化',
+                link: './css解决方案/CSS规范化.md',
+              }],
+            },
+          ],
+          '/reference/': [
+            {
+              text: 'Reference',
+              children: ['/reference/cli.md', '/reference/config.md'],
+            },
+          ],
+    }, //侧边栏
+    repo: "https://github.com/Hello-GBY",
+    repoLabel: "GitHub",
     displayAllHeaders: false, //展开侧边栏所有的小标题
     sidebarDepth: 1, //提取markdown中h2标题，作为小标题显示在侧边栏上。
-    lastUpdated: '最后更新', // 文档更新时间：每个文件git最后提交的时间,
+    lastUpdated: "最后更新", // 文档更新时间：每个文件git最后提交的时间,
   }),
   head: [
-    //         // 这里的 '/' 指向 docs/.vuepress/public 文件目录 
-      ['link', {
-          rel: 'icon',
-          href: '/img/witch.ico' //配置网页的小图标，如下图
-      }]
+    //         // 这里的 '/' 指向 docs/.vuepress/public 文件目录
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "/img/witch.ico", //配置网页的小图标，如下图
+      },
+    ],
   ],
-    markdown: {
-        lineNumbers: true // 代码块显示行号
-    },
-  description: '这是我的第一个 VuePress 站点',
-})
+  markdown: {
+    lineNumbers: true, // 代码块显示行号
+  },
+  description: "这是我的第一个 VuePress 站点",
+});
 
 // module.exports = {
 //     description: "正在搭建中...",
 //     head: [
-//         // 这里的 '/' 指向 docs/.vuepress/public 文件目录 
+//         // 这里的 '/' 指向 docs/.vuepress/public 文件目录
 //         ['link', {
 //             rel: 'icon',
 //             href: '/img/witch.ico' //配置网页的小图标，如下图
--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,41 +1,23 @@
 const nav = [
-    // 1.0 仅显示标题到导航栏
     {
-        text: '首页',
-        link: '/'
+        text: '一起进步',
+        link: '/一起进步/一起进步.md'
     },
-    // 2.0 显示标题到导航栏，且显示下拉标题
     {
-        text: '关于作者',
-        children: [{
-                text: 'GitHub',
-                link: '',
-            },
-            {
-                text: '掘金',
-                link: '',
-            }
-        ]
+        text: '企业级实战经验',
+        link: '/dev-experience/企业级实战经验.md'
     },
-    // 3.0 显示标题到导航栏，且分类别显示下拉标题
     {
-        text: '面试笔记',
-        items: [{
-                text: '基础',
-                //此处/以docs为根目录
-                items: [{
-                        text: 'Javascript',
-                        link: '/前端/01.Javascript/01.正则表达式',
-                        // sidebarDepth: 0,
-                    },
-                    {
-                        text: 'ES6',
-                        link: '/PAGE/2019-10-14-js-reg/',
-                        // link: '/前端/02.ES6/',
-                    },
-                ]
-            },
-        ]
+        text: '源码原理',
+        link: '/源码原理/源码原理.md'
+    },
+    {
+        text: '个人知识库',
+        link: '/个人知识库/个人知识库.md'
+    },
+    {
+        text: '读书笔记',
+        link: '/读书笔记/读书笔记.md'
     }
 ]
 
