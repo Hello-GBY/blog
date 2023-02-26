fde014007775cecc7aa06d63cee9d4e7324f0f88
gby <nameGBY@163.com>
2023-02-26 18:45:14 +0800

    feat(导航)：去掉源码章节

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,24 +1,25 @@
 const nav = [
-    {
-        text: '一起进步',
-        link: '/一起进步/一起进步.md'
-    },
-    {
-        text: '企业级实战经验',
-        link: '/dev-experience/企业级实战经验.md'
-    },
-    {
-        text: '源码原理',
-        link: '/源码原理/源码原理.md'
-    },
-    {
-        text: '个人知识库',
-        link: '/个人知识库/个人知识库.md'
-    },
-    {
-        text: '读书笔记',
-        link: '/读书笔记/读书笔记.md'
-    }
-]
+  {
+    text: "一起进步",
+    link: "/一起进步/一起进步.md",
+  },
+  {
+    text: "企业级实战经验",
+    link: "/dev-experience/企业级实战经验.md",
+  },
+
+  {
+    text: "个人知识库",
+    link: "/个人知识库/个人知识库.md",
+  },
+  {
+    text: "读书笔记",
+    link: "/读书笔记/读书笔记.md",
+  },
+  {
+    text: "掘金",
+    link: "https://juejin.cn/user/976780341553960/posts",
+  },
+];
 
 module.exports = nav;
